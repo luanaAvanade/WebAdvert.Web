518234882d47f5581cf1651f9365de4978a078c0
Luana Rodrigues Santos <l.rodrigues.santos@avanade.com>
2021-09-30 17:37:40 -0700

    Authentication With AWS

---
+++ WebAdvert.web/WebAdvert.web/Controllers/Accounts.cs
@@ -0,0 +1,117 @@
+﻿using Amazon.Extensions.CognitoAuthentication;
+using Microsoft.AspNetCore.Identity;
+using Amazon.AspNetCore.Identity.Cognito;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAdvert.web.Models.Accounts;
+using Amazon.Runtime.Internal.Transform;
+
+namespace WebAdvert.web.Controllers
+{
+    public class Accounts : Controller
+    {
+        private readonly SignInManager<CognitoUser> _signInManager;
+        private readonly UserManager<CognitoUser> _userManager;
+        private readonly CognitoUserPool _pool;
+
+        public Accounts(SignInManager<CognitoUser> signInManager, UserManager<CognitoUser> userManager, CognitoUserPool pool)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _pool = pool;
+        }
+
+        public async Task<IActionResult> Signup()
+        {
+            var model = new SignupModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signup(SignupModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _pool.GetUser(model.Email);
+
+                if (user.Status != null)
+                {
+                    ModelState.AddModelError("UserExists", "User with this email already exists");
+                    return View(model);
+                }
+
+                user.Attributes.Add(CognitoAttribute.Name.AttributeName, model.Email);
+                var createUser = await _userManager.CreateAsync(user, model.Password).ConfigureAwait(false);
+                if (createUser.Succeeded)
+                {
+                    RedirectToAction("Confirm");
+                }
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> Confirm(ConfirmModel model)
+        {
+            //var model = new ConfirmModel();
+            return View(model);
+        }
+
+        public async Task<IActionResult> Confirm_Post(ConfirmModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email).ConfigureAwait(false);
+                if (user == null)
+                {
+                    ModelState.AddModelError("NotFound", "A user with the given email address was not found");
+                    return View(model);
+                }
+
+                var result = await _userManager.ConfirmEmailAsync(user, model.Code).ConfigureAwait(false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError(item.Code, item.Description);
+                    }
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Login")]
+        public async Task<IActionResult> Login_Post(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("LonginError", "Email and password do not match");
+                }
+            }
+            return View("Login", model);
+        }
+
+    }
+}
